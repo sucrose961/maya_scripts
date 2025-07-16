dfd444c03e0a601a0c9f9da08cb6687f5133fa31
sucrose961 <sucrose2025@outlook.kr>
2025-07-16 15:20:41 +0900

    v0003

--- create_table.py
+++ create_table.py
@@ -12,11 +12,11 @@ def create_table(table_size_x, table_size_y, table_size_z, leg_size_x, leg_size_
         cmds.move(table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, table_size_z*0.5*z_bias, leg0, relative=True)
         # Duplicate a leg
         leg1 = cmds.duplicate('leg0')[0]
-        cmds.setAttr(leg1 + '.translate', -table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, table_size_z*0.5*y_bias, type = 'double3')
+        cmds.setAttr(leg1 + '.translate', -table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, table_size_z*0.5*z_bias, type = 'double3')
         # Duplicate a legs
         leg2, leg3 = cmds.duplicate('leg0', 'leg1')
-        cmds.setAttr(leg2 + '.translate', table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, -table_size_z*0.5*y_bias, type = 'double3')
-        cmds.setAttr(leg3 + '.translate', -table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, -table_size_z*0.5*y_bias, type = 'double3')
+        cmds.setAttr(leg2 + '.translate', table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, -table_size_z*0.5*z_bias, type = 'double3')
+        cmds.setAttr(leg3 + '.translate', -table_size_x*0.5*x_bias, (-leg_size_y-table_size_y)*0.5, -table_size_z*0.5*z_bias, type = 'double3')
         # Grouping
         cmds.group(top, leg0, leg1, leg2, leg3, n='table')
     else:
