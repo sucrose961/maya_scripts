48822b1d74e899ebf7bd5282a40dceab5b6359e5
sucrose961 <sucrose2025@outlook.kr>
2025-07-18 15:12:53 +0900

    v001

---
+++ subtract_ymin.py
@@ -0,0 +1,18 @@
+import maya.cmds as cmds
+
+def subtract_ymin(obj):
+	if cmds.objExists(obj):
+		# Get a BBOX attribute
+		bbox = cmds.exactWorldBoundingBox(obj)
+		min_y = bbox[1]
+		
+		# Get a World Space Position
+		pos = cmds.xform(obj, q=True, ws=True, t=True)
+		
+		# Subtract min_y
+		new_pos = [pos[0], pos[1]-min_y, pos[2]]
+		
+		# Set xform attribute
+		cmds.xform(obj, ws=True, t=new_pos)
+	else:
+		print(f"'{obj}' is not Exist")
